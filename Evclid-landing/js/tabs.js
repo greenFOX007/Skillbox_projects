0569ae2d4f159e186f04cf84ba27ae6276f53c97
DESKTOP-4BJOFMG\vova2 <vova280297@gmail.com>
2021-06-29 11:52:33 +0300

    Готовое домашнее задание

---
+++ Evclid-landing/js/tabs.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelectorAll('.step-slider__link').forEach(function(tabsBtn) {
+    tabsBtn.addEventListener('click', function(event) {
+      const path = event.currentTarget.dataset.path
+      document.querySelectorAll('.tab-content').forEach(function(tabContent) {
+        tabContent.classList.remove('tab-content-active')
+      })
+      document.querySelector(`[data-target="${path}"]`).classList.add('tab-content-active')
+    })
+  })
+
+
+})
